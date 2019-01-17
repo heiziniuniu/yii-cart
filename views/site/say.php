e288e0b933e6bf44f01410524a3d214e029a34c6
firstBlackCat <sundandan@ehaier.com>
2019-01-17 11:06:12 +0800

    修改readme

--- views/site/say.php
+++ views/site/say.php
@@ -1,7 +1,7 @@
 <?php
 
 /* @var $this yii\web\View */
-
+use yii\helpers\Html;//前面是忘了加这句，报错。。。
 $this->title = 'My Yii Application';
 // $request = Yii::$app->request; 
 // $get = $request->get();
@@ -9,5 +9,6 @@ $this->title = 'My Yii Application';
 // print_r($str);
 ?>
 <div>
-    <h1><?=  Yii::$app->request->get('message')?></h1>
+    <h1>通过URL请求参数获取的：<?=  Yii::$app->request->get('message')?></h1>
+    <h2>通过URL返回的参数获取的：<?= Html::encode($message) ?></h2>
 <div>
